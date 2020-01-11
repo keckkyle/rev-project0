7943a7189546ca20c578d43c46fb0e22e8c0a0d7
Kyle Keck <keckkyle@gmail.com>
2020-01-11 10:56:53 -0500

    Create User, Employee, Customer and Car classes

---
+++ src/main/java/com/revature/pojos/Customer.java
@@ -0,0 +1,13 @@
+package com.revature.pojos;
+
+public class Customer extends User {
+
+	public Customer() {
+		super();
+	}
+
+	public Customer(String name, String username, String password) {
+		super(name, username, password);
+	}
+
+}
---
+++ src/main/java/com/revature/pojos/Employee.java
@@ -0,0 +1,13 @@
+package com.revature.pojos;
+
+public class Employee extends User {
+
+	public Employee() {
+		super();
+	}
+
+	public Employee(String name, String username, String password) {
+		super(name, username, password);
+	}
+
+}
